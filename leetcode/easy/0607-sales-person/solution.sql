5941df490e5085ad4c6d17864869bf3ad59661ab
Veeresh Hanni <164135881+Veeresh-Hanni@users.noreply.github.com>
2026-08-05 20:30:29 +0530

    ✅ Sales Person | Easy | SQL | Runtime: 1806 ms (15.98%) | Memory: 0B | 2026-08-05

--- leetcode/easy/0607-sales-person/solution.sql
+++ leetcode/easy/0607-sales-person/solution.sql
@@ -1,13 +1,5 @@
 # Write your MySQL query statement below
 
-
--- SELECT s.name from SalesPerson s
--- JOIN Orders o ON s.sales_id = o.sales_id
--- Join Company c ON c.com_id= o.com_id
--- WHERE c.name = 'RED' 
-# Write your MySQL query statement below
-
-
 SELECT s.name FROM SalesPerson s
 WHERE s.sales_id NOT IN (
     SELECT s.sales_id from SalesPerson s
