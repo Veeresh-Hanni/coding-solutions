f0eb8061773020c3d32831caa93893cf8320edf6
Veeresh Hanni <164135881+Veeresh-Hanni@users.noreply.github.com>
2026-08-26 11:39:24 +0530

    ✅ Subarray Division | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-26

--- hackerrank/medium/the-birthday-bar/solution.py
+++ hackerrank/medium/the-birthday-bar/solution.py
@@ -19,29 +19,29 @@ import sys
 def birthday(s, d, m):
     
     # Approach 1 Brite force
-    count = 0
-    n = len(s)
+    # count = 0
+    # n = len(s)
     
-    for idx in range(n - m + 1):
-        if sum(s[idx:idx+m]) == d:
-            count += 1
-    return count
+    # for idx in range(n - m + 1):
+    #     if sum(s[idx:idx+m]) == d:
+    #         count += 1
+    # return count
     
     # Approach 2 sliding window
-    # count = 0
-    # window_sum = sum(s[:m])
+    count = 0
+    window_sum = sum(s[:m])
 
-    # if window_sum == d:
-    #     count += 1
+    if window_sum == d:
+        count += 1
 
-    # for i in range(m, len(s)):
-    #     window_sum += s[i]       # add right
-    #     window_sum -= s[i - m]  # remove left
+    for i in range(m, len(s)):
+        window_sum += s[i]       # add right
+        window_sum -= s[i - m]  # remove left
 
-    #     if window_sum == d:
-    #         count += 1
+        if window_sum == d:
+            count += 1
 
-    # return count    
+    return count    
     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
