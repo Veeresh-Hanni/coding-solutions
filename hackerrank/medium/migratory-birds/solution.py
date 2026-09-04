aef14f9bb4b66fe3b83fce871fc8d7db08367247
Veeresh Hanni <164135881+Veeresh-Hanni@users.noreply.github.com>
2026-09-04 20:11:20 +0530

    ✅ Migratory Birds | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-04

--- hackerrank/medium/migratory-birds/solution.py
+++ hackerrank/medium/migratory-birds/solution.py
@@ -11,20 +11,35 @@ import os
 
 def migratoryBirds(arr):
     # Write your code here
-    freq = {}
+    # freq = {}
     
-    for a in arr:
-        freq[a] = freq.get(a, 0) + 1
+    # for a in arr:
+    #     freq[a] = freq.get(a, 0) + 1
     
     
-    max_id = 0
-    max_val = float('-inf')
-    for k, v in freq.items():
+    # max_id = 0
+    # max_val = float('-inf')
+    # for k, v in freq.items():
 
-        if v > max_val or (v == max_val and k < max_id):
-            max_id = k
-            max_val = v
-    return max_id
+    #     if v > max_val or (v == max_val and k < max_id):
+    #         max_id = k
+    #         max_val = v
+    # return max_id
+    
+    freq = [0] * 6
+
+    for bird in arr:
+        freq[bird] += 1
+
+    max_count = 0
+    answer = 0
+
+    for bird in range(1, 6):
+        if freq[bird] > max_count:
+            max_count = freq[bird]
+            answer = bird
+
+    return answer
     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
