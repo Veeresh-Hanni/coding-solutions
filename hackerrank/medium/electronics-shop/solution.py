16a3aa385db75bc3a1d642f9c1594353a3cd50af
Veeresh Hanni <164135881+Veeresh-Hanni@users.noreply.github.com>
2026-09-11 20:41:18 +0530

    ✅ Electronics Shop | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-11

--- hackerrank/medium/electronics-shop/solution.py
+++ hackerrank/medium/electronics-shop/solution.py
@@ -1,30 +1,53 @@
 #!/bin/python3
+
 import os
+import sys
 
 #
-# Complete the 'pageCount' function below.
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER p
+# Complete the getMoneySpent function below.
 #
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    
+    expensive = 0
+    
+    d_idx = 0
+    n = len(drives)
+    while d_idx < n:
+        for k in range(len(keyboards)):
+            total = drives[d_idx] + keyboards[k] 
+            if total <= b and total > expensive:
+                
+                expensive = total
+                
+                 
+        d_idx += 1
+    return -1 if expensive < 1 else expensive
 
-def pageCount(n, p):
-    # Write your code here
-    front = p // 2
-    back = (n // 2) - front
-    
-    return min(front, back)
-    
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input().strip())
+    bnm = input().split()
 
-    p = int(input().strip())
+    b = int(bnm[0])
 
-    result = pageCount(n, p)
+    n = int(bnm[1])
 
-    fptr.write(str(result) + '\n')
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
 
     fptr.close()
